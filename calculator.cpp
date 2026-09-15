0b0a95e7b391bfa623ba44f6a45b3f530a5cb9c3
AMAN YADAV <codewithaman13@gmail.com>
2026-09-15 12:32:01 +0530

    added operator fn

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
+    double a,b;
     cin>> a,b;
-    
+    cout<<"what do u wanna do:"<<endl;
+    cout<<"1 - add"<<endl<<"2 - substract\n"<<" 3 - multiply\n"<<" 4- division\n ";
+    int option;
+    cin>>option;
+    switch(option){
+        case 1: add(a,b);
+        case 2: sub(a,b);
+        case 3: mul(a,b);
+        case 4: div(a,b);
+    }
 }
