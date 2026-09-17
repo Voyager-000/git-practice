e5ecd9b5894800f252426aa8658bda0995608d29
AMAN YADAV <codewithaman13@gmail.com>
2026-09-17 15:19:46 +0530

    made 2sum file

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 using namespace std;
+double add(double a, double b){
+    return a+b;
+}
+double sub( double a, double b){
+    return a-b;
+}
+double mul( double a, double b){
+    return a*b;
+}
+double div(double a, double b){
+    return a/b;
+}
 int main(){
     double a,b;
-    cin>> a,b;
+    cin>> a>>b;
     cout<<"what do u wanna do:"<<endl;
     cout<<"1 - add"<<endl<<"2 - substract\n"<<" 3 - multiply\n"<<" 4- division\n ";
     int option;
     cin>>option;
     switch(option){
-        case 1: add(a,b);
-        case 2: sub(a,b);
-        case 3: mul(a,b);
-        case 4: div(a,b);
+        case 1: cout<< add(a,b); break;
+        case 2: cout<<sub(a,b); break;
+        case 3: cout<<mul(a,b);break;
+        case 4: cout<<div(a,b);break;
     }
 }
